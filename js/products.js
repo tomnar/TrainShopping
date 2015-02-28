4dd38afa6644a1888fa42fe268be1f3628a3452f
btwotch <btwotch+github@gmail.com>
2015-02-28 21:36:24 +0100

    json: more products and another category

--- js/products.js
+++ js/products.js
@@ -44,37 +44,37 @@ products = [{
         "img": "7.png"
     }, {
         "id": 8,
-        "name": "two",
-        "price": 31.33,
-        "description": "this is awesome too",
-        "img": "placeholder"
+        "name": "Kartoffeln festkochend",
+        "price": 0.15,
+        "description": "Speisefrühkartoffeln müssen vollständig reif geerntet und sofort nach der Rodung vermarktet werden.",
+        "img": "8.png"
     }]
 }, {
-    "category": "meat",
+    "category": "Hygiene",
     "items": [{
-        "id": 1,
-        "name": "one",
-        "price": 33.33,
-        "description": "this is awesome",
-        "img": "placeholder"
+        "id": 9,
+        "name": "Tempo Feuchte Toilettentücher mit Kamille 42 Stück",
+        "price": 1.95,
+        "description": "Tempo feuchte Toilettentücher sind die ideale Ergänzung zu trockenem Toilettenpapier - für ein sicheres Gefühl von Sauberkeit.",
+        "img": "9.png"
     }, {
-        "id": 2,
-        "name": "two",
-        "price": 31.33,
-        "description": "this is awesome too",
-        "img": "placeholder"
+        "id": 10,
+        "name": "Rexona men Cobalt 150ml",
+        "price": 1.59,
+        "description": "REXONA AE COBALT",
+        "img": "10.png"
     }, {
-        "id": 3,
-        "name": "one",
-        "price": 33.33,
-        "description": "this is awesome",
-        "img": "placeholder"
+        "id": 11,
+        "name": "Isana Deo Zerstäuber",
+        "price": 2.99,
+        "description": "ISANA Deo Zerstäuber Sensitiv mit 24h-Wirkkomplex schützt zuverlässig und langanhaltend vor Körpergeruch und gibt Ihnen optimale Sicherheit für den ganzen Tag.",
+        "img": "11.png"
     }, {
-        "id": 4,
-        "name": "two",
-        "price": 31.33,
-        "description": "this is awesome too",
-        "img": "placeholder"
+        "id": 12,
+        "name": "o.b. Original Normal 32 Stück",
+        "price": 3.39,
+        "description": "Zuverlässiger Schutz.",
+        "img": "12.png"
     }, {
         "id": 5,
         "name": "one",
