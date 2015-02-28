4f252217d53becc857170faf012487f28247722a
btwotch <btwotch+github@gmail.com>
2015-02-28 21:02:37 +0100

    json: first products

--- js/products.js
+++ js/products.js
@@ -1,1 +1,409 @@
-products = [{"category":"meat", "items":[{"id":1,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":2,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":3,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":4,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":5,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":6,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":7,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":8,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"}]},{"category":"meat", "items":[{"id":1,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":2,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":3,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":4,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":5,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":6,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":7,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":8,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"}]},{"category":"meat", "items":[{"id":1,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":2,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":3,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":4,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":5,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":6,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":7,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":8,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"}]},{"category":"meat", "items":[{"id":1,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":2,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":3,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":4,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":5,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":6,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":7,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":8,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"}]},{"category":"meat", "items":[{"id":1,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":2,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":3,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":4,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":5,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":6,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":7,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":8,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"}]},{"category":"meat", "items":[{"id":1,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":2,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":3,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":4,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":5,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":6,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":7,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":8,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"}]},{"category":"meat", "items":[{"id":1,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":2,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":3,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":4,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":5,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":6,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":7,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":8,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"}]},{"category":"meat", "items":[{"id":1,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":2,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":3,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":4,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":5,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":6,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"},{"id":7,"name":"one","price":33.33,"description":"this is awesome","img":"placeholder"},{"id":8,"name":"two","price":31.33,"description":"this is awesome too","img":"placeholder"}]}]
+products = [{
+    "category": "eat",
+    "items": [{
+        "id": 1,
+        "name": "Sandwich Club",
+        "price": 2.99,
+        "description": "Ursprungsland: Deutschland",
+        "img": "1.png"
+    }, {
+        "id": 2,
+        "name": "Teigwaren Riesa GmbH Goldtraum Bunte Nudeln 500g",
+        "price": 1.89,
+        "description": "Teigwaren Riesa GmbH Goldtraum Bunte Nudeln 500g",
+        "img": "2.png"
+    }, {
+        "id": 3,
+        "name": "REWE Bio Pasta Soße 330ml",
+        "price": 1.99,
+        "description": "Tomate-Basilikum",
+        "img": "3.png"
+    }, {
+        "id": 4,
+        "name": "Chio Chips Red Paprika 175g",
+        "price": 1.99,
+        "description": " Chio Chips Red Paprika 175g",
+        "img": "4.png"
+    }, {
+        "id": 5,
+        "name": "HARIBO Goldbären",
+        "price": 0.95,
+        "description": "",
+        "img": "5.png"
+    }, {
+        "id": 6,
+        "name": "Lindt Mini Pralines Nougat 165g",
+        "price": 8.99,
+        "description": "",
+        "img": "6.png"
+    }, {
+        "id": 7,
+        "name": "Harry Vollkorn Urtyp 500g",
+        "price": 1.29,
+        "description": "",
+        "img": "7.png"
+    }, {
+        "id": 8,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }]
+}, {
+    "category": "meat",
+    "items": [{
+        "id": 1,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 2,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 3,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 4,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 5,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 6,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 7,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 8,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }]
+}, {
+    "category": "meat",
+    "items": [{
+        "id": 1,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 2,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 3,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 4,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 5,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 6,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 7,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 8,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }]
+}, {
+    "category": "meat",
+    "items": [{
+        "id": 1,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 2,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 3,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 4,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 5,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 6,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 7,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 8,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }]
+}, {
+    "category": "meat",
+    "items": [{
+        "id": 1,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 2,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 3,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 4,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 5,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 6,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 7,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 8,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }]
+}, {
+    "category": "meat",
+    "items": [{
+        "id": 1,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 2,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 3,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 4,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 5,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 6,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 7,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 8,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }]
+}, {
+    "category": "meat",
+    "items": [{
+        "id": 1,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 2,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 3,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 4,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 5,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 6,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 7,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 8,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }]
+}, {
+    "category": "meat",
+    "items": [{
+        "id": 1,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 2,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 3,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 4,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 5,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 6,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }, {
+        "id": 7,
+        "name": "one",
+        "price": 33.33,
+        "description": "this is awesome",
+        "img": "placeholder"
+    }, {
+        "id": 8,
+        "name": "two",
+        "price": 31.33,
+        "description": "this is awesome too",
+        "img": "placeholder"
+    }]
+}]
