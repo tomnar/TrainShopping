9348bca8dd68f95dacc455ac735fa8ad0947ed92
tomnar <tomquast89@gmail.com>
2015-02-28 20:34:31 +0100

    working with scrollview logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 $( document ).ready(function() {
 
+    var productView = $('.productView');
+    for(var i = 0; i < products.length; i++){
+        var cat = products[i];
+        var catName = cat.category;
+        var catItems = cat.items;
+
+        var innerHtml = "";
+        for(var u = 0; u < catItems.length; u++){
+            var item = catItems[u];
+            var itemId = item.id;
+            var itemName = item.name;
+            var itemPrice = item.price;
+            var itemDescription = item.description;
+            var itemImg = item.img;
+            innerHtml += "<image src=\"img/placeholder_product.jpg\"></image>";
+        }
+
+        productView.append(
+            "<div class=\"hScroll\">" +
+                "<h2>" + catName + "</h2>" +
+                "<div>" +
+                    innerHtml +
+                "</div>" +
+            "</div>"
+        );
+    }
 });
